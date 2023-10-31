a3d820d8b946acc8b58853814ec2ac80a695d03a
Luxy <146051503+lusy17@users.noreply.github.com>
2023-10-31 13:30:04 +0800

    Kuloah Algo 31 Oktober 2023

---
+++ 01-ProgramJavaPertama/src/App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //Program Biodata Mahasiswa
+        String nama;
+        String nim;
+        String tgl_lahir;
+        String alamat;
+        float berat_badan;
+        float tinggi_badan;
+
+        //mengisi variabel 
+        nama = "lusi";
+        nim = "23241048";
+        tgl_lahir = "17 feberuari 2006";
+        alamat = "bima";
+        berat_badan = 36f;
+        tinggi_badan = 145f; 
+
+        // cetak biodata
+       System.out.println("nama : " + nama);
+        System.out.println("nim :" + nim);
+         System.out.println("tgl_lahir :" + tgl_lahir);
+          System.out.println("alamat :" + alamat);
+           System.out.println("berat_badan :" + berat_badan);
+            System.out.println("tinggi_badan :" + tinggi_badan);
+
+        
+    }
+}   
